e14c155224597908770abf206e92f5a3599064ee
Mpho Mutchinya <mutchinyampho@gmail.com>
2026-05-13 12:51:40 +0200

    App config

---
+++ app/main.py
@@ -0,0 +1,10 @@
+# 0 is no default(low risk), 1 is default(high risk)
+
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+ 
+st.set_page_config(page_title="Credit Default Interactive Model", layout="wide")
