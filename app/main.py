17ecac0be8a9d7c8dc6d1e9c40e7f911e484585b
Mpho Mutchinya <mutchinyampho@gmail.com>
2026-05-14 09:07:53 +0200

    Heading and sidebar app

--- app/main.py
+++ app/main.py
@@ -7,4 +7,25 @@ import seaborn as sns
 import numpy as np
 
  
-st.set_page_config(page_title="Credit Default Interactive Model", layout="wide")
+st.set_page_config(page_title="Credit Default Interactive Dashboard", layout="wide")
+
+st.title("Credit Default Interactive Dashboard")
+
+#create sidebar
+
+def render_sidebar():
+    with st.sidebar:
+        st.markdown("Credit Interactive Dashboard")
+        st.markdown("FNB Dataquest 2026")
+        st.divider()
+
+        st.markdown("**Navigation**")
+        st.page_link("main.py", label="Home")
+        st.page_link("pages/1_EDA.py", label="EDA Tool")
+        st.page_link("pages/2_Model.py", label="Model evaluation")
+        st.page_link("pages/3_Dashboard.py", label="Business Dashboard")
+
+
+
+
+
